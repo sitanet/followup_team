089bf4e3db24c76e6e2dfb0f8afd7d10d73e4c10
Bayo <sitanetglobaltech@gmail.com>
2023-07-14 12:58:25 +0100

    storages_issues

--- followup_city/settings.py
+++ followup_city/settings.py
@@ -201,7 +201,17 @@ AWS_S3_FILE_OVERWRITE = config('AWS_S3_FILE_OVERWRITE')
 AWS_DEFAULT_ACL = config('AWS_DEFAULT_ACL')
 AWS_S3_VERITY = config('AWS_S3_VERITY')
 # STATICFILES_STORAGES = 'storages.backends.s3boto3.S3Boto3Storage'
-STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}}
+# STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}}
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
